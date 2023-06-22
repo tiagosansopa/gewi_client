7c35742aabf3e464eab7aabc6c8645eab74f5105
tiagosansopa <tiagosansopa@gmail.com>
2023-06-22 11:08:33 -0600

    feat: Access basic redirect logic implemented
    pending:
    validate id
    design

--- pages/key/[id].js
+++ pages/key/[id].js
@@ -1,7 +1,31 @@
 import { useState } from "react";
+import { useRouter } from "next/router";
 import { keyStyles } from "../../styles";
 import axios from "axios";
 const Key = ({ access }) => {
+  const router = useRouter();
+  const updateContact = async (contactUpdated) => {
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_NAME}/contact/valid`,
+        contactUpdated
+      );
+
+      console.log(response.data.message);
+      router.push(`/key/${response.data.contactId}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleValidate = () => {
+    //validar que el ID image es valido...
+    const contactUpdated = {
+      name: "hola",
+    };
+    updateContact(contactUpdated);
+  };
+
   return (
     <div className={keyStyles.container}>
       <div>
@@ -12,7 +36,7 @@ const Key = ({ access }) => {
           </p>
         </div>
       </div>
-      <form className={keyStyles.form}>
+      <form onSubmit={handleValidate} className={keyStyles.form}>
         <label>Nombre</label>
         <input value={access.contact.name}></input>
         <label>Apellido</label>
